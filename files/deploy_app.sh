5c134b0a31c1688507c17d540a375d098549d7a4
Michael Davis <askmikeyd@gmail.com>
2022-03-24 23:51:35 +0000

    new slogan

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Coles Liquor! 
+  <center><h3>You can't say no to these eyes.</h3></center>
   <!-- END -->
   
   </div>
