9f165922c216228e25e61a11e1c646b6b38e05a8
Michael Davis <askmikeyd@gmail.com>
2022-03-25 00:47:25 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,14 +1,5 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "Guarav"
-  location = "West Europe"
-}
-
 module "network" {
   source  = "app.terraform.io/askmikeyd/network/azurerm"
   version = "3.5.0"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = "Guarav"
 }
